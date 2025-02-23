cafa8164279e9d60ad81732ba7ff5a554055f986
Ross Klaiber <124365723+RossKlaiber@users.noreply.github.com>
2025-02-22 19:35:05 -0500

    Update BioDataJSON.py

--- BioDataJSON.py
+++ BioDataJSON.py
@@ -1,11 +1,13 @@
 import requests
 import json
 from bs4 import BeautifulSoup
+import time
 
 def extract_fencer_data(url):
     """
     Extracts static fencer details using requests and BeautifulSoup.
     """
+
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
     
@@ -17,6 +19,24 @@ def extract_fencer_data(url):
     # Extract Name
     name_tag = soup.find('h1', class_='AthleteHero-fencerName')
     data['Name'] = name_tag.get_text(strip=True) if name_tag else None
+
+    #Extract country id
+    nationality_div = soup.find('div', class_='AthleteHero-nationality')
+    span = nationality_div.find('span') if nationality_div else None
+    span_classes = span.get('class') if span else []
+    #the 3rd element contains the country code, after the "--"
+    result = span_classes[2].split('--')[-1]
+    data["Flag"] = result
+    
+    # Extract left / right handness
+    hand_span = soup.find('span', string='Hand')
+    next_span = hand_span.find_next('span') if hand_span else None
+    data["Handedness"] = next_span.get_text(strip=True) if next_span else None
+
+    # Extract age
+    age_span = soup.find('span', string='Age')
+    next_span = age_span.find_next('span') if age_span else None
+    data["Age"] = next_span.get_text(strip=True) if next_span else None
     
     # Extract Image URL
     image_div = soup.find('div', class_='AthleteHero-fencerImage')
@@ -36,8 +56,8 @@ def extract_fencer_data(url):
     rank_tag = soup.find('span', class_='ProfileInfo-rank')
     data['Rank'] = rank_tag.get_text(strip=True) if rank_tag else None
 
-    rank_tag = soup.find('span', class_='AthletBio-stat test')
-    data['Residence'] = rank_tag.get_text(strip=True) if rank_tag else None
+    residence_tag = soup.find('span', class_='AthletBio-stat test')
+    data['Residence'] = residence_tag.get_text(strip=True) if residence_tag else None
 
     # Extract Points
     pts_label = soup.find(lambda tag: tag.name == 'span' and 'Pts' in tag.get_text())
